b5c6ae4dc7846eb2ded4c989a939e163d3c46b26
ading2210 <ading2019@gmail.com>
2024-10-24 06:25:17 -0400

    compile wolfssl with more reasonable options

--- client/tools/wolfssl.sh
+++ client/tools/wolfssl.sh
@@ -17,7 +17,7 @@ cd wolfssl
 
 autoreconf -fi
 export CFLAGS="-Oz -DSP_WORD_SIZE=32 -DWOLFSSL_NO_ATOMICS -DWOLFSSL_MAX_ALT_NAMES=1024" 
-emconfigure ./configure --prefix=$PREFIX --enable-curl --enable-static --disable-shared --host=i686-linux --disable-examples --disable-asm --enable-sni --enable-alpn --enable-truncatedhmac --enable-oldtls --enable-tlsv12 --enable-all-crypto --disable-asyncthreads --disable-threadlocal --enable-tlsx 
+emconfigure ./configure --prefix=$PREFIX --enable-curl --enable-static --disable-shared --host=i686-linux --disable-examples --disable-asm --enable-sni --enable-alpn --enable-truncatedhmac --enable-tlsv12 --enable-all-crypto --disable-arc4 --disable-asyncthreads --disable-threadlocal --enable-tlsx --disable-nullcipher 
 emmake make -j$CORE_COUNT
 make install
 
